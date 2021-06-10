fd808ae85ddab35c39adebfc38f05194680d5efb
Your Name <jagoba.ruizab@elorrieta-errekamari.com>
2021-06-10 13:01:30 +0200

    Importar Articulo en articulos-component.ts

--- src/app/articulos/articulos.component.ts
+++ src/app/articulos/articulos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Articulo } from '../articulo';
+
 
 @Component({
   selector: 'app-articulos',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articulos.component.css']
 })
 export class ArticulosComponent implements OnInit {
+articulos: Articulo[] = [];
 
   constructor() { }
 
